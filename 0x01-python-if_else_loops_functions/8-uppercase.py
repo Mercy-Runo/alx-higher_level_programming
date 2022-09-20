f66828551e12160100f9abfe2f046443b5bc7b23
Mercy-Runo <mercyruno37@gmail.com>
2022-09-20 13:38:19 -0700

    compressed code forlesser variables

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        letter = str[i]
-        tmp = ord(letter)
-        if tmp >= 97 and tmp < 123:
-            tmp -= 32
-            #letter = chr(tmp)
+    for i in (str):
+        if ord(i) >= 97 and ord(i) < 123:
+            i = chr(ord(i) - 32)
         else:
             continue
-    print("{}".format(letter))
+    print("{}".format(i), end='')
+    print()
