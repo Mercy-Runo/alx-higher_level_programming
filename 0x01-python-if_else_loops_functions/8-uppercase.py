1dad867289447c20114fc89898e279e05ea9d9d6
Mercy-Runo <mercyruno37@gmail.com>
2022-09-20 13:27:23 -0700

    attemp to clear typesript error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,10 @@
 def uppercase(str):
     for i in range(len(str)):
         letter = str[i]
-        if ord(letter) >= 97 and ord(letter) < 123:
-            ord(letter) -= 32
+        tmp = ord(letter)
+        if tmp >= 97 and tmp < 123:
+            tmp -= 32
+            letter = chr(tmp)
         else:
             continue
     print("{}".format(letter))
